24cae51491870e3cd145d5edaa2d957f84245b09
DShpak27 <mr.shpak@gmail.com>
2023-02-13 20:51:05 +0200

    1212121

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { HashRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 import { store } from './redux/store';
@@ -11,12 +11,14 @@ import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-        <HashRouter>
+        <BrowserRouter basename="goit-react-hw-08-phonebook">
+            {/* <HashRouter> */}
             <Provider store={store}>
                 <PersistGate loading={null} persistor={persistor}>
                     <App />
                 </PersistGate>
             </Provider>
-        </HashRouter>
+        </BrowserRouter>
+        {/* </HashRouter> */}
     </React.StrictMode>
 );
